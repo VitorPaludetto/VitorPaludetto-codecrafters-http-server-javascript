91aa6db84bddf7a7e79a45bd9476c875937db256
Vitor Paludetto <vitorhugopaludetto@outlook.com>
2024-04-24 15:58:43 -0300

    pass 2nd stage

--- app/main.js
+++ app/main.js
@@ -9,6 +9,13 @@ const server = net.createServer((socket) => {
     socket.end();
     server.close();
   });
+
+  socket.on("data", (data) => {
+    console.log(data);
+    socket.write("HTTP/1.1 200 OK\r\n\r\n");
+})
+
 });
+
 
 server.listen(4221, "localhost");
