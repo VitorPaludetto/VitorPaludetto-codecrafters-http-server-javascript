91baf541a2fba913771f6823e9f912d5e8d6fdc9
Vitor Paludetto <vitorhugopaludetto@outlook.com>
2024-04-25 15:18:49 -0300

    fix path checking when is needed to send body response

--- app/main.js
+++ app/main.js
@@ -26,13 +26,12 @@ const server = net.createServer((socket) => {
     const firstLine = input.split("\n")[0];
     const path = firstLine.split(" ")[1];
 
-    const matchEchoRegex = /(\/echo\/)(.*)/
-    const echoPath = path.match(matchEchoRegex)[2];
-
-    console.log(echoPath);
-    console.log(echoPath.length);
-
-    if (path === "/" || path.substring(0, 5) === "/echo") {
+    if (path === "/") {
+      socket.write("HTTP/1.1 200 OK\r\n\r\n");
+    }
+    else if (path.substring(0, 5) === "/echo") {
+      const matchEchoRegex = /(\/echo\/)(.*)/
+      const echoPath = path.match(matchEchoRegex)[2];
       socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${echoPath.length}\r\n\r\n${echoPath}\r\n`);
     } else {
       socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
