518bf467019bba3f7314d0d837a15389fd9069ca
Vitor Paludetto <vitorhugopaludetto@outlook.com>
2024-04-25 15:39:46 -0300

    pass 5th stage

--- app/main.js
+++ app/main.js
@@ -7,7 +7,9 @@ const server = net.createServer((socket) => {
   });
 
   // Extract the Path from the HTTP request
-  // If "/" respond with 200 OK, else respond with 404 Not Found
+  // If "/" respond with 200 OK, 
+  // If starts with "/echo", respond with the rest of the path as the body
+  // else respond with 404 Not Found
 
     // Example of incoming HTTP request (404 Not Found)
     // GET /index.html HTTP/1.1
@@ -33,7 +35,13 @@ const server = net.createServer((socket) => {
       const matchEchoRegex = /(\/echo\/)(.*)/
       const echoPath = path.match(matchEchoRegex)[2];
       socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${echoPath.length}\r\n\r\n${echoPath}\r\n`);
-    } else {
+    } 
+    else if (path.startsWith("/user-agent")) {
+      const userAgentRegex = /(User-Agent: )(.*)/
+      const userAgent = input.match(userAgentRegex)[2]
+      socket.write(`HTTP/1.1 200 OK\r\nContent-Type: plain/text\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}\r\n`)
+    }
+    else {
       socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
     }
   })
